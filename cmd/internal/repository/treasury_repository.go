958150bc8284d34e7795c5e3e48df09157d02438
Pablo Santos <pablorodrigo552@gmail.com>
2024-12-31 16:28:43 -0300

    feat: add treasury api repository

---
+++ cmd/internal/repository/treasury_repository.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/pablorodrigo52/transaction-api/cmd/internal/model"
+)
+
+type TreasuryRepository interface {
+	GetExchangeRateByCountry(country string) (*model.TreasuryRatesExchange, error)
+}
+
+type TreasuryRepositoryImpl struct {
+	Domain string
+	Path   string
+	Client http.Client
+	log    *slog.Logger
+}
+
+func NewTreasuryRepository(domain, path string, timeout time.Duration, log *slog.Logger) *TreasuryRepositoryImpl {
+	return &TreasuryRepositoryImpl{
+		Domain: domain,
+		Path:   path,
+		Client: http.Client{
+			Timeout: timeout,
+		},
+		log: log,
+	}
+}
+
+func (r *TreasuryRepositoryImpl) GetExchangeRateByCountry(country string) (*model.TreasuryRatesExchange, error) {
+
+	url := fmt.Sprintf(
+		"%s%s?fields=record_date,country,exchange_rate,currency,effective_date&filter=country:eq:%s&sort=-record_date&page[number]=1&page[size]=1&format=json",
+		r.Domain,
+		r.Path,
+		country,
+	)
+
+	resp, err := r.Client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		r.log.Error("Error on execute treasury api call", "status_code", resp.StatusCode, "response", resp.Body)
+		return nil, fmt.Errorf("treasury api call error [status_code:%d]", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var exchangeRateResponse model.TreasuryRatesExchange
+	if err := json.Unmarshal(body, &exchangeRateResponse); err != nil {
+		return nil, err
+	}
+
+	return &exchangeRateResponse, nil
+}
---
+++ cmd/internal/repository/treasury_repository_test.go
@@ -0,0 +1,98 @@
+package repository
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/pablorodrigo52/transaction-api/cmd/internal/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetExchangeRateByCountry_APICall(t *testing.T) {
+	tests := []struct {
+		name           string
+		country        string
+		mockResponse   string
+		mockStatusCode int
+		expectedError  error
+		expectedResult *model.TreasuryRatesExchange
+	}{
+		{
+			name:           "Should execute call with success",
+			country:        "Brazil",
+			mockResponse:   `{"data": [{"record_date": "2024-09-30","country": "Brazil","exchange_rate": "5.434","currency": "Real","effective_date": "2024-09-30"}]}`,
+			mockStatusCode: http.StatusOK,
+			expectedError:  nil,
+			expectedResult: &model.TreasuryRatesExchange{
+				Data: []model.Data{
+					{
+						RecordDate:    "2024-09-30",
+						Country:       "Brazil",
+						ExchangeRate:  "5.434",
+						Currency:      "Real",
+						EffectiveDate: "2024-09-30",
+					},
+				},
+				Meta: nil,
+			},
+		},
+		{
+			name:           "Should return an error because status code is different from 200",
+			country:        "Brazil",
+			mockResponse:   `{"error":"not found"}`,
+			mockStatusCode: http.StatusNotFound,
+			expectedError:  errors.New("treasury api call error [status_code:404]"),
+			expectedResult: nil,
+		},
+		{
+			name:           "Should return an error because invalid JSON response",
+			country:        "Brazil",
+			mockResponse:   `invalid json`,
+			mockStatusCode: http.StatusOK,
+			expectedError:  errors.New("invalid character 'i' looking for beginning of value"),
+			expectedResult: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockServer := httptest.NewServer(
+				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(tt.mockStatusCode)
+					w.Write([]byte(tt.mockResponse))
+				}))
+			defer mockServer.Close()
+
+			repo := NewTreasuryRepository(
+				mockServer.URL,
+				"/services/api/fiscal_service/v1/accounting/od/rates_of_exchange",
+				20*time.Millisecond,
+				slog.Default(),
+			)
+			result, err := repo.GetExchangeRateByCountry(tt.country)
+
+			if err != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tt.expectedError.Error(), err.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedResult, result)
+			}
+		})
+	}
+}
+
+func Test_GetExchangeRateByCountry_Client(t *testing.T) {
+
+	t.Run("GetExchangeRateByCountry error on create client", func(t *testing.T) {
+		treasuryRepository := NewTreasuryRepository("http://127.0.0.1", "\u2342", 1*time.Second, slog.Default())
+
+		_, err := treasuryRepository.GetExchangeRateByCountry("Brazil")
+
+		assert.Error(t, err)
+	})
+}
